7ad4fa912b137e2bd3e898036ef8b132a0a85880
sandee sharma <sandeep06362014@gmail.com>
2020-06-27 16:27:57 +0530

    initial author

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 // loading env variables
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/author');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 //mongodb connection
 mongoose.connect(process.env.ATLAS_URI, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -22,6 +25,7 @@ db.once('open', () => console.log('connected to mongoose'));
 
 // routes
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 
 app.listen(process.env.PORT || 3000);
